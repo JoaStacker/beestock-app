6365913f6c64a87323e0416b71b46970aab3747f
matiasgastonlonghino@gmail.com <matiasgastonlonghino@gmail.com>
2024-10-08 18:42:59 -0300

    :Un poco más avanzado en el login
    hay algunos inconsistencia que estoy arreglando pero va quedando documentado el login.

--- crud/src/main/java/com/api/crud/config/ValidationConfig.java
+++ crud/src/main/java/com/api/crud/config/ValidationConfig.java
@@ -3,7 +3,8 @@ package com.api.crud.config;
 import com.api.crud.services.models.validation.UserValidation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+//esta clase es necesaria al establecer en models la carpeta valdiation y consecuentemente una validacion de usuario
+//es necesaria esta clase puesto que estamos creando un objeto y dicha creacion y manejo es llevada a cabo por un @bean
 @Configuration
 public class ValidationConfig {
     @Bean
