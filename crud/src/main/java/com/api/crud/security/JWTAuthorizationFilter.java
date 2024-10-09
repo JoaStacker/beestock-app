d8a0ef03f831e10fc05e43caf6ea2b84792b64ab
matiasgastonlonghino@gmail.com <matiasgastonlonghino@gmail.com>
2024-10-08 21:28:30 -0300

    :Estructura general arreglada

--- crud/src/main/java/com/api/crud/security/JWTAuthorizationFilter.java
+++ crud/src/main/java/com/api/crud/security/JWTAuthorizationFilter.java
@@ -18,11 +18,13 @@ import java.security.spec.InvalidKeySpecException;
 import java.text.ParseException;
 import java.util.Collections;
 
+
 public class JWTAuthorizationFilter extends OncePerRequestFilter {
 
-    @Autowired
+    //@Autowired   tal vez haga falta pero hay un constructor ya creado
     IJWTUtilityService jwtUtilityService;
 
+    // Constructor que permite la inyección de la implementación de IJWTUtilityService.
     public JWTAuthorizationFilter(IJWTUtilityService jwtUtilityService) {
         this.jwtUtilityService = jwtUtilityService;
     }
@@ -30,20 +32,21 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String header = request.getHeader("Authorization");//no tiene que ser name:   ?
-        if(header == null || !header.startsWith("Bearer ")) {//bearer y el espacio. Ojo
-            filterChain.doFilter(request, response);
+        if (header == null || !header.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response); // Continúa sin autenticación.
             return;
         }
-        String token = header.substring(7);
-        try{
+
+        String token = header.substring(7); // Extrae el token JWT.
+        try {
+            // Valida el token y obtiene los reclamos (claims).
             JWTClaimsSet claims = jwtUtilityService.parseJWT(token);
+            // Crea el objeto de autenticación.
             UsernamePasswordAuthenticationToken authentication =
                     new UsernamePasswordAuthenticationToken(claims.getSubject(), null, Collections.emptyList());
-
+            // Establece la autenticación en el contexto de seguridad.
             SecurityContextHolder.getContext().setAuthentication(authentication);
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        } catch (InvalidKeySpecException e) {
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
             throw new RuntimeException(e);
         } catch (ParseException e) {
             throw new RuntimeException(e);
