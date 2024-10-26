ab94eb23e8a894c1c171bb638cc4789aed92d6de
Joaquin E. Sarmiento <joaquin.sarmiento@ieee.org>
2024-10-26 17:33:04 -0300

    Ignorar target and .idea 3

--- crud/src/main/java/com/api/crud/persistence/repositories/ILocalidadRepository.java
+++
@@ -1,9 +0,0 @@
-package com.api.crud.persistence.repositories;
-
-import com.api.crud.persistence.entities.Localidad;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ILocalidadRepository extends JpaRepository<Localidad, Long> {
-}
--- crud/src/main/java/com/api/crud/services/IPuestoService.java
+++
@@ -1,8 +0,0 @@
-package com.api.crud.services;
-
-import com.api.crud.services.models.dtos.HistorialPuestoDTO;
-import org.springframework.http.ResponseEntity;
-
-public interface IPuestoService {
-    public ResponseEntity<Object> findAll() throws Exception;
-}
