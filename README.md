b473f3dc2101e26ecd82de9364cb477e1b591e4c
Joaquin E. Sarmiento <joaquin.sarmiento@ieee.org>
2024-10-26 17:30:25 -0300

    Ignorar target and .idea 2

--- README.md
+++
@@ -1,1 +0,0 @@
-# beestock-app
--- beestock-front/src/adapters/dataAdapter.js
+++
@@ -1,9 +0,0 @@
-export const adaptData = (data) => {
-    // Transform data as needed
-    return data.map(item => ({
-      id: item.id,
-      name: item.name,
-      // More transformations if needed
-    }));
-  };
-  
